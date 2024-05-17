e982bbc9e2ed65ee081dd19f96b4b31963321775
Prof Blanc <profblanc@gmail.com>
2024-05-17 10:38:43 -0400

    wk2 lab- Fri @ 9am

---
+++ lab/wk2/fri@9am/first.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print("Hello")
+print(True)
+print(123456789)
+print(123.456)
+
+# +  single-line comment
+"""
+multi-line comments
+"""
---
+++ lab/wk2/fri@9am/inputting_out_outputting.py
@@ -0,0 +1,13 @@
+"""
+Create a script that introduces self.
+Ask user for their name
+Greet user: Hello, {their name}
+"""
+# print("Hello, my name is Ben")
+# name = input("What is your name? ")
+# print("Hello,", name)
+print("I am 100 years old.")
+age = input("How old are? ")  # always returns a string
+age = int(age)  # int(input("how old are you"))
+print("Your age is ", age)
+print("Next year, you will be", age + 1, "years old")

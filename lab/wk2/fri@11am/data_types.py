658298bbc61db84db7f4a4a2351a49f82fe64098
Prof Blanc <profblanc@gmail.com>
2024-05-17 12:48:38 -0400

    wk2 lab- Fri @ 11am

---
+++ lab/wk2/fri@11am/data_types.py
@@ -0,0 +1,22 @@
+"""
+name_of_variable = value
+    string
+    int
+    float
+    bool(ean)
+"""
+name = "Ben Blanc"
+address = "160 Kendal Ave"
+
+loves_python = True
+hates_the_delays_on_the_TTC = True
+
+num1 = 1
+num2 = 1.1
+num3 = 0x1f  # hex
+print(num3)
+num4 = 0b1110111
+num5= 0o127
+
+print(num1 + num2)
+print(num3 + num4)

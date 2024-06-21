d81d0ee0a1a3cb25a0a4dba60538ac6865932c33
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2024-06-21 11:45:04 -0400

    Update lab.py

--- lab/wk7/mon@4pm/lab.py
+++ lab/wk7/mon@4pm/lab.py
@@ -43,3 +43,28 @@ result = sleep("bed")
 print(result)
 result = sleep("train")
 print(result)
+
+
+# determine which string variable is the longest (accept 2 or 3 values)
+
+def which_is_longest(s1, s2, s3=""):
+    # s1 = str(s1)
+    # s2 = str(s2)
+    # s3 = str(s3)
+
+    if isinstance(s1, str) and isinstance(s2, str) and isinstance(s3, str):
+        values = [s1, s2, s3]
+        # values.sort(key=len)
+        # return values[-1]
+        values.sort(key=len, reverse=True)
+        return values[0]
+
+result1 = which_is_longest("hello", "good", "i love python")
+result2 = which_is_longest("hello", "good")
+# result1 = "i love python"
+# result2 = "hello"
+print(result1)
+print(result2)
+
+result3 = which_is_longest(1234, 124.56, 32452)
+print(result3)

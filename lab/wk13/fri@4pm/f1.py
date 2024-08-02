1ebf5acf394a8100566553ce4accab127f47f48c
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2024-08-02 16:52:44 -0400

    Create f1.py

---
+++ lab/wk13/fri@4pm/f1.py
@@ -0,0 +1,11 @@
+from super_hero import SuperHero
+batman = SuperHero(human_name="Bruce Wayne",
+                   hero_name="Batman",
+                   hero_power="money",
+                   hero_planet="Earth")
+
+print(batman)
+
+double_batman = batman * "5"
+
+print(double_batman)

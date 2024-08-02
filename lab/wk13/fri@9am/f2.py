f483798e6be3d94c6a5db32cdf43afb4e30f1930
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2024-08-02 11:06:21 -0400

    Create f2.py

---
+++ lab/wk13/fri@9am/f2.py
@@ -0,0 +1,10 @@
+from super_hero import SuperHero
+
+batman = SuperHero("Batman", 15)
+catwoman = SuperHero("Catwoman", 20)
+
+print(batman)
+print(catwoman)
+
+baby = batman + catwoman
+print(baby)

338c1037dfe7047a96c349757960d1736c19c292
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2024-08-09 12:48:01 -0400

    Create f3.py

---
+++ lab/wk14/fri@9am/f3.py
@@ -0,0 +1,36 @@
+import requests
+
+site_url = "https://YOUR.gblearn.com"
+
+
+
+
+response = requests.get(site_url, auth=('username', 'ghdgdg'))
+
+print(response.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
